21676b070d52c9b6f91e5aa9b07772584a3c1ebb
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-06 12:15:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const strongElement = document.getElementsByTagName("strong");
 
 function highlight() {
     for(const strong of strongElement){
-		strong.style.color="rgb(0, 128, 0)";
+		// strong.style.color="rgb(0, 128, 0)";
+		strong.classList.add("highlighted");
 	}
 }
 function return_normal() {
     for(const strong of strongElement){
-		strong.style.color= "black";
+		// strong.style.color= "black";
+		strong.classList.remove("highlighted");
 	}
 }
 
